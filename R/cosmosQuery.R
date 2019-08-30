d92339cd4eddab2e87c9b352c37ac1404927d2fd
David Balderree <recursor@gmail.com>
2019-08-30 13:40:00 -0500

    Fixed continuation code Added function for using continuation to pull entire collection

--- R/cosmosQuery.R
+++ R/cosmosQuery.R
@@ -1,5 +1,5 @@
 #' POST a full query to the REST API for Cosmos DB.
-#' 
+#'
 #' @param sql.what String for specifying what fields to retrieve. Typically called select condition. Defaults to *
 #' @param sql.where String for specifying what filter to use on data. Typically called search condition. Defaults to empty.
 #' @param max.items Number of documents to retrieve per page
@@ -7,13 +7,15 @@
 #' @param debug.auth Logical value for getting verbose output of auth header being constructed. Defaults to false.
 #' @param debug.query Logical value for getting verbose output of HTTP response, printing all headers. Defaults to false.
 #' @param content.response Logical value to determine whether to retrieve full response or just the documents
+#' @param content.flatten Logical value to determine whether to flatten the data frame returned. Only performed when content.response = TRUE
+#' @param content.removecosmos Logical value to determine whether to remove Cosmos DB columns from the returned data frame. Only performed when content.response = TRUE
 #' @return Prints status code of HTTP POST, and returns full HTTP response or just the content
 #' @keywords query cosmosdb post
 #' @export
 #' @examples
 #' cosmosQuery(sql.what = "c.contact.eloquaId", sql.where = "c.contact.eloquaId != null")
 
-cosmosQuery <- function(sql.what = "*", sql.where = "", max.items = 100, continuation = FALSE, debug.auth = FALSE, debug.query = FALSE, content.response = FALSE) {
+cosmosQuery <- function(sql.what = "*", sql.where = "", max.items = 100, continuation = FALSE, debug.auth = FALSE, debug.query = FALSE, content.response = TRUE, content.flatten = content.response, content.removecosmos = content.response) {
 
     require(digest)
     require(base64enc)
@@ -48,14 +50,12 @@ cosmosQuery <- function(sql.what = "*", sql.where = "", max.items = 100, continu
     auth.header <- genHeader(verb = "POST", resource.type = res.type, resource.link = res.link, stored.time = ms.date.string, debug = debug.auth)
 
     # Assemble headers for the POST
-    all.headers <- c("Authorization" = auth.header, "x-ms-version" = "2017-02-22", "x-ms-date" = ms.date.string, "Content-Type" = "application/query+json", "x-ms-documentdb-isquery" = "true", "x-ms-documentdb-query-enablecrosspartition" = "true", "x-ms-max-item-count" = max.items)
-
+    all.headers <- c("Authorization" = auth.header, "x-ms-version" = "2018-12-31", "x-ms-date" = ms.date.string, "Content-Type" = "application/query+json", "x-ms-documentdb-isquery" = "true", "x-ms-documentdb-query-enablecrosspartition" = "true", "x-ms-max-item-count" = max.items)
     # Check for continuation, if attempting to get pages
     if (continuation == TRUE) {
-        if (!is.null(envCosmosDB$continuation$token)) {
-            continuation.token <- c("x-ms-continuation-token" = envCosmosDB$continuation$token)
+        if (!is.null(get0("continuation",envir = envCosmosDB))) {
+            continuation.token <- c("x-ms-continuation" = get0("continuation",envir = envCosmosDB))
             all.headers <- append(all.headers, continuation.token)
-            print(all.headers)
             rm(continuation, envir = envCosmosDB)
         }
     }
@@ -68,7 +68,6 @@ cosmosQuery <- function(sql.what = "*", sql.where = "", max.items = 100, continu
 
     # Extract continuation headers and token, store outside of function scope for next call
     continuation.header <- response.headers$`x-ms-continuation`
-    continuation.header <- fromJSON(continuation.header)
     assign("continuation", continuation.header, envir = envCosmosDB)
 
     # Send the status code of the POST to the console
@@ -85,11 +84,36 @@ cosmosQuery <- function(sql.what = "*", sql.where = "", max.items = 100, continu
     if (content.response == FALSE) {
         raw.response
     } else if (content.response == TRUE) {
-        char.response <- readContent(raw.response)
-        char.response$Documents
+        char.response <- as.data.frame(readContent(raw.response)$Documents)
+        if(content.flatten == TRUE)
+            char.response = flatten(char.response)
+        if(content.removecosmos == TRUE)
+            char.response = subset(char.response, select = -c(`id`, `_rid`, `_self`, `_ts`, `_etag`, `_attachments`, `_ts`))
+
+        char.response
     } else {
         print("Invalid content response option specified. Logical value required.")
     }
+}
 
+#' POST a full query to the REST API for Cosmos DB which returns all rows.
+#'
+#' @param sql.what String for specifying what fields to retrieve. Typically called select condition. Defaults to *
+#' @param sql.where String for specifying what filter to use on data. Typically called search condition. Defaults to empty.
+#' @return Returns the content
+#' @keywords query cosmosdb post
+#' @export
+#' @examples
+#' cosmosQueryAll(sql.what = "c.contact.eloquaId", sql.where = "c.contact.eloquaId != null")
 
+cosmosQueryAll <- function(sql.what = "*", sql.where = "") {
+    require(dplyr)
+    allrecords <- data.frame()
+    repeat{
+        result <- cosmosQuery(sql.what = sql.what, sql.where = sql.where, continuation = TRUE, content.removecosmos = FALSE)
+        allrecords <- bind_rows(allrecords, result)
+        if (is.null(get0("continuation",envir = envCosmosDB)))
+            break
+    }
+    allrecords
 }
